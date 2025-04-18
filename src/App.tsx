d8bd9cbf219e1cfc421c2732a250d4bd8004de43
Shwetha R K <Shwetha.RK@readynetworks.com>
2025-04-18 17:42:49 +0530

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { HashRouter } from 'react-router-dom'; // Note 1
 
 import { ThemeProvider } from './contexts/ThemeContext';
